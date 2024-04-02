a12e4f40d281c75c2a7245c3cbdfce239c6b6e84
formaggia <luca.formaggia@gmail.com>
2024-04-02 13:07:40 +0200

    comments

--- Source/Amijo.cpp
+++ Source/Amijo.cpp
@@ -7,7 +7,7 @@ double Amijo::calculate_step(const vector &xk, const int iter) const{
         double ak=a0;
         for (int i=0; i<max_iter;++i){
             vector s{v::scalar_mult(grf(fun,xk,h),a0)};
-            double normsq(pow(v::calculate_norm(grf(fun,xk,h)),2));
+            double normsq(pow(v::calculate_norm(grf(fun,xk,h)),2));//@note don't use pow to elevate to power 2. Multiply the number by itself
             if(fun(xk)-fun(v::substract(xk,s))>=(sigma*a0*normsq)){
                 break;
             }else{
